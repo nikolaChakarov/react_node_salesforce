2c51be88b84ba55e53bb190bea81f4cc10774279
Nikola Chakarov <nikola.chakarov.dev@gmail.com>
2023-11-11 13:21:20 +0200

    connect Node with Apex @RestResource

--- client/src/submit-form/submit-form.js
+++ client/src/submit-form/submit-form.js
@@ -3,9 +3,11 @@ import { GlobalContext } from '../context/app-context';
 import styled from 'styled-components';
 
 const SubmitForm = () => {
-    const { handleUploadDataToSalesforce } = useContext(GlobalContext);
+    const { handleUploadDataToSalesforce, token } = useContext(GlobalContext);
     const nameRef = useRef();
     const fileRef = useRef();
+
+    const [apexContarcts, setApexContracts] = useState([]);
 
     const handleSubmit = async () => {
         const data = {
@@ -14,6 +16,42 @@ const SubmitForm = () => {
         };
 
         handleUploadDataToSalesforce(data);
+    };
+
+    // const handleFetchRecordList = async () => {
+    //     const dbRes = await fetch('http://localhost:5005/sf/sobject/single', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             token: '00D06000000yu0j!ARIAQFGj1sIEeNRnwTf1BV8YW7tRJKNRbudcLtpkksUq_sQ_eH5MMBvfN6HHHy8onGxC6u8DqeH3RJqwvHE5I0CgtHRHKDWU',
+    //             id: '8000600000BZhTCAA1',
+    //         }),
+    //     });
+
+    //     const res = await dbRes.json();
+    //     console.log(res);
+    // };
+
+    const handleFetchRecordList = async () => {
+        const dbRes = await fetch('http://localhost:5005/sf/sobject/apex', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                token:
+                    token ||
+                    '00D06000000yu0j!ARIAQFGj1sIEeNRnwTf1BV8YW7tRJKNRbudcLtpkksUq_sQ_eH5MMBvfN6HHHy8onGxC6u8DqeH3RJqwvHE5I0CgtHRHKDWU',
+                id: '8000600000BZhTCAA1',
+                apexPath: '/services/apexrest/api/webhooks/',
+            }),
+        });
+
+        const { message, payload } = await dbRes.json();
+        console.log(payload.objList);
+        setApexContracts((prev) => [...payload.objList]);
     };
 
     return (
@@ -42,6 +80,25 @@ const SubmitForm = () => {
             <div className='btn-wrapper'>
                 <button onClick={handleSubmit}>Submit to Salesforce</button>
             </div>
+
+            <div className='btn-wrapper'>
+                <button onClick={handleFetchRecordList}>
+                    Fetch Contracts List From SF
+                </button>
+            </div>
+
+            <hr />
+            {apexContarcts.length
+                ? apexContarcts.map((ctr, idx) => (
+                      <p key={idx}>
+                          Contact Id: <span>{ctr.Id}</span>, Related Account
+                          Name:
+                          <span>{ctr.Account.Name}</span>, Start Date:
+                          <span>{ctr.StartDate}</span>, End Date:
+                          <span>{ctr.EndDate}</span>
+                      </p>
+                  ))
+                : null}
         </SubmitStyled>
     );
 };
@@ -55,6 +112,10 @@ const SubmitStyled = styled.div`
         display: flex;
         flex-direction: column;
         gap: 5px;
+    }
+
+    span {
+        font-weight: bold;
     }
 `;
 
--- server/routes/sobject.js
+++ server/routes/sobject.js
@@ -3,22 +3,42 @@ import jsforce from 'jsforce';
 
 export const getSObject = express.Router();
 
-getSObject.post('/', (req, res, next) => {
+getSObject.post('/single', (req, res, next) => {
     const { token, id } = req.body;
+
+    console.log(token);
 
     const conn = new jsforce.Connection({
         instanceUrl: process.env.SF_INSTANCE,
         accessToken: token,
     });
 
-    // Single record retrieval
-    conn.sobject('Student__c').retrieve(id, function (err, account) {
+    conn.sobject('Contract').retrieve(id, function (err, contract) {
         if (err) {
             return console.error(err);
         }
-        console.log('Name : ' + account.Student_Name__c);
-        // ...
+        console.log(contract);
+        res.status(200).json({ message: 'ok', payload: contract });
+    });
+});
+
+getSObject.post('/apex', (req, res, next) => {
+    const { token, apexPath } = req.body;
+
+    const conn = new jsforce.Connection({
+        instanceUrl: process.env.SF_INSTANCE,
+        accessToken: token,
     });
 
-    res.json({ message: 'ok' });
+    // body payload structure is depending to the Apex REST method interface.
+    // var body = { title: 'hello', num: 1 }; // for POST
+    conn.apex.get(apexPath, function (err, ap) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('response: ', ap);
+        // the response object structure depends on the definition of apex class
+
+        res.status(200).json({ message: 'ok', payload: ap });
+    });
 });
