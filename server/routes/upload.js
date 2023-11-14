1c83c9f6f866866e9de6e41a3d2f1ba7a56f102c
Nikola Chakarov <nikola.chakarov.dev@gmail.com>
2023-11-14 16:46:20 +0200

    successfully upload file from Salesforce to Node.js API

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
 import jsforce from 'jsforce';
+import fs from 'fs';
 
 export const upload = express.Router();
 
+// end point reached from React and Postman
 upload.post('/apex-file', async (req, res) => {
     // values come from formData since we want to send a file and more information in the body;
     const { salesforceAuthToken, apexPath } = req.body;
@@ -20,26 +22,41 @@ upload.post('/apex-file', async (req, res) => {
     const file = req.files.current;
     const fileName = file.name;
 
+    // const buf = Buffer.from(file.data);
+    // console.log(buf.toString());
+
     const uploadPath = __dirname + '/uploads/' + fileName;
     file.mv(uploadPath);
 
     // go for APEX!!
 
     // body payload structure is depending to the Apex REST method interface.
-    var body = { title: 'FILE UPLOAD TEST', file: file };
-    conn.apex.post(apexPath, body, function (err, res) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('response: ', res);
-        // the response object structure depends on the definition of apex class
-    });
+    // const body = { title: 'FILE UPLOAD TEST', file: file };
+    // conn.apex.post(apexPath, body, function (err, res) {
+    //     if (err) {
+    //         return console.error(err);
+    //     }
+    //     console.log('response: ', res);
+    //     // the response object structure depends on the definition of apex class
+    // });
 
     res.status(200).json({ msg: 'ok!!!' });
 });
 
+// // end point reached from Salesforce
 upload.post('/api', async (req, res) => {
-    console.log('req.body:', req.files);
+    // apexFile is set from apex code. it can be anything.
+    const file = req.files.apexFile;
+    const filename = file.name;
 
-    res.send('ok!');
+    const convertedFileData = atob(file.data);
+
+    const uploadPath = __dirname + '/uploads/' + filename;
+    // file.mv(uploadPath);
+
+    // fs.writeFileSync("10111.pdf", body,'binary');
+
+    fs.writeFileSync(uploadPath, convertedFileData, 'binary');
+
+    res.status(200).json({ msg: 'file successfully received!' });
 });
